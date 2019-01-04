f7382ced88cadb3c4a9a1d1171c4832566dd4556
adexphem <adexphem4real@gmail.com>
2019-01-04 12:37:12 +0100

    introduce waiting loader and contact listing

--- App.vue
+++ App.vue
@@ -3,10 +3,11 @@
     <view class="bar"></view>
     <view class="container">
       <image
-        :style="{width: 200, height: 200}"
+        :style="{width: 300, height: 300}"
         :source="{uri: 'https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/S/O/145816_1545587421.jpg'}"
       ></image>
       
+      <!-- form - input contact -->
       <view class="form-wrapper">
         <text>Add New Member</text>
         <text-input
@@ -20,26 +21,56 @@
         </touchable-opacity>
       </view>
 
-      <view class="flex-container header">
-        <text class="white-text first">Medium</text>
-        <text class="white-text others">Email Address</text>
-      </view>
+      <!-- display contacts -->
+      <view class="wrapper gap-up-10">
+        <view class="flex header-tilt">
+          <text :style="{color: '#33b27b', fontWeight: '600'}">MEMBER LIST</text>
+        </view>
 
-      <scroll-view class="contact-wrapper" >
-        <view v-for="(contact, key) in contactList" :key="key">
-          <view class="flex-container contact-list">
-            <text class="first">{{ contact.invitation_medium }}</text>
-            <text class="others">{{ contact.email }}</text>
+        <view class="contact-wrapper" >
+          <view v-if="loading" class="loader-wrapper">
+            <image :source="loader" class="loader" />
+          </view>
+
+          <view v-else v-for="(contact, key) in contactList" :key="key">
+            <view class="contact-list card">
+              <!-- //top -->
+              <view class="flex">
+                <view>
+                  <image
+                  class="img-wrapper"
+                  :source="{uri: 'https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/S/O/145816_1545587421.jpg'}"
+                ></image>
+                </view>
+                <view class="top-wrap">
+                  <text class="sub" :style="{color: 'rgba(33, 33, 33, 0.9)', fontWeight: '600', fontSize: 15}">{{ contact.email }}</text>
+                  <view class="flex">
+                    <text class="title" :style="{fontWeight: '300'}">Medium: </text>
+                    <text class="title" :style="{color: '#33b27b', fontWeight: '300'}">{{ contact.invitation_medium }}</text>
+                  </view>
+                </view>
+              </view>
+
+              <!-- //details -->
+              <view>
+                <text class="title grey-text">{{ contact.address }}</text>
+              </view>
+
+              <!-- //footer -->
+              <view class="flex" :style="{ marginTop: 10, justifyContent: 'flex-end'}">
+                <text class="ctn-edit">Edit</text>
+              </view>
+            </view>
           </view>
         </view>
-      </scroll-view>
+      </view>
     </view>
   </ScrollView>
-  
 </template>
 
 <script>
 import axios from 'axios'
+import loader from './assets/loader.gif'
 
 const AUTH_TOKEN = `BearereyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoyfQ.Qtxy5mDBDF4y_s8TlWPTHAiEKPvI1crqhZM1zjfg7Ys`;
 axios.defaults.baseURL = 'https://qontactsky.herokuapp.com/';
@@ -50,31 +81,44 @@ export default {
   data() {
     return {
       memberName: null,
-      contactList: []
+      contactList: [],
+      loading: true,
+      loader: loader
     };
   },
   methods: {
     onTapSubmit() {
-      this.contactList = '';
+      axios.post(`contact`, {
+        email: this.memberName
+        }).then(result => {
+          console.log("result ",result);
+      }).catch(error => {
+        console.log("failed ", error);
+      }).finally();
+      this.memberName = '';
+    },
+    loadContacts() {
+      axios.get(`contact`).then(result => {
+        this.contactList = result.data.contacts;
+        this.loading = false;
+      }).catch(error => {
+        console.log("failed ", error);
+      }).finally()
     }
   },
   mounted() {
-    axios.get(`contact`).then(result => {
-      this.contactList = result.data.contacts;
-    }).catch(error => {
-      console.log("failed ", error);
-    }).finally()
+    this.loadContacts();
   }
 };
 </script>
  
 <style>
 .container {
-  background-color: #ffffff;
+  background-color: #f5f5f5;
   align-items: center;
   justify-content: center;
   flex: 1;
-  padding: 20;
+  padding: 10;
 }
 .text-color-primary {
   color: red;
@@ -97,36 +141,69 @@ export default {
   color: white;
   font-size: 100;
 }
-.header {
-  width: 98%;
-  background-color: rgb(2, 38, 26);
-  padding-top: 10;
-  padding-bottom: 10;
+.center {
+  text-align: center;
 }
 .form-wrapper {
   margin-top: 10;
   margin-bottom: 10;
 }
+.wrapper {
+  width: 100%;
+  height: 100%;
+  flex: 1;
+}
+.img-wrapper {
+  width: 40;
+  height: 40;
+  border-radius: 35;
+}
+.gap-up-10 {
+  margin-top: 25;
+}
 .contact-wrapper {
-  width: 98%;
-  height: 100;
+  width: 100%;
+  height: 100%;
+}
+.loader-wrapper {
+  flex-direction: row;
+  justify-content: center;
+  padding: 10;
 }
 .contact-list {
   padding-top: 10;
   padding-bottom: 10;
-  border-color: rgba(97, 97, 97, 0.2);
-  border-bottom-width: 1;
+  margin-top: 10;
 }
-.flex-container {
+.grey-text {
+  color: rgba(111, 111, 111, 0.995);
+  font-size: 13;
+}
+.card {
+  border-radius: 5;
+  border-width: 1;
+  border-color: rgba(210, 210, 210, 0.6);
+  background-color: #ffffff;
+  padding: 10;
+}
+.card .title {
+  font-weight: 400;
+  font-size: 30;
+}
+.card .title {
+  font-weight: 200;
+  font-size: 20;
+}
+.flex {
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  flex: 1;
 }
-.flex-container .first {
+.flex .first {
   text-align: left;
   flex: 1;
 }
-.flex-container .others {
+.flex .others {
   text-align: left;
   flex: 2;
 }
@@ -134,5 +211,31 @@ export default {
   width: 100%;
   background-color: #33b27b;
   padding: 10;
+}
+.loader {
+  width: 100;
+  height: 100;
+}
+.header-tilt {
+  background-color: #f5f5f5;
+  border-color: #a4c2b5;
+  justify-content: center;
+  border-width: 1;
+  padding: 10;
+}
+.top-wrap {
+  padding-left: 10;
+}
+.top-wrap .title {
+  color: rgb(33, 33, 33);
+  font-size: 70;
+}
+.ctn-edit {
+  border-color: #a4c2b5;
+  color: #a4c2b5;
+  border-width: 1;
+  padding: 8;
+  text-align: center;
+  min-width: 120;
 }
 </style>
